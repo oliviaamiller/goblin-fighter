368d7948e511608b9ff2e0ce6f93701b519f7288
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-12-09 17:05:14 -0800

    added DOM elements to js

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 // import functions and grab DOM elements
+const defeatedNumEl = document.querySelector('#defeated-number');
+const defeatedListEl = document.querySelector('.defeated-list');
+const dogImgEl = document.querySelector('#dog-img');
+const dogHPEl = document.querySelector('#dog-hp');
+const ghostListEl = document.querySelector('.ghosts');
+const form = document.querySelector('form');
+
+
+
 
 // let state
 
@@ -6,3 +15,16 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+
+function displayDog() {
+// write this function 
+
+}
+
+function displayGhosts() {
+// write this function
+}
+
+displayDog();
+displayGhosts();
