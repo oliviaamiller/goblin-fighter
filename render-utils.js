ad51921099d341c680709c73949f73bbbf7fb1e7
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-12-09 18:02:04 -0800

    started writing renderGhost function

---
+++ render-utils.js
@@ -0,0 +1,18 @@
+export function renderGhost(ghost) {
+    const ghostDiv = document.createElement('div');
+    const nameEl = document.createElement('p');
+    const ghostImgEl = document.createElement('img');
+    const ghostHPEl = document.createElement('p');
+
+
+    ghostDiv.classList.add('ghost-div');
+    nameEl.classList.add('ghost-name');
+    ghostImgEl.classList.add('ghost-img');
+    ghostHPEl.classList.add('ghost-hp');
+
+    
+
+
+
+
+}
