e3d8a6b50dc89da884a65016a70cf45cd2369a31
oliviaamiller <oliviaoliviaolivia@gmail.com>
2021-12-13 09:35:22 -0800

    has bugs in if statements, need to work on that

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { renderGhost } from './render-utils.js';
 const defeatedNumEl = document.querySelector('#defeated-number');
 const dogImgEl = document.querySelector('#dog-img');
 const dogHPEl = document.querySelector('#dog-hp');
+const dogStrengthEl = document.querySelector('#dog-strength');
 const ghostsEl = document.querySelector('.ghosts');
 const form = document.querySelector('form');
 
@@ -11,6 +12,7 @@ const form = document.querySelector('form');
 // let state
 let defeatedGhostsCount = 0;
 let playerHP = 10;
+let playerStrength = 0;
 let ghosts = [
     { 
         name: 'Ron', 
@@ -80,83 +82,131 @@ function displayGhosts() {
         if (ghost.hp > 0) {
             eachGhost.addEventListener('click', () => {
                 if (ghost.agility === 1) {
-                    if (Math.random() < .5) { 
-                        ghost.hp--;
-                        alert(`You zapped ${ghost.name}!`);
-                    } else {
-                        alert(`You tried to strike ${ghost.name} but missed, try again!`);
+                    if (Math.random() < .25) { 
+                        if (playerStrength === 0) {
+                            ghost.hp--;
+                            alert(`You zapped ${ghost.name}!`);
+                        } else {
+                            alert(`You tried to strike ${ghost.name} but missed, try again!`);
+                        }
+
+                        if (playerStrength === 1) {
+                            ghost.hp -= 2;
+                            alert(`You're getting stronger, you really clobbered ${ghost.name}!`);
+                        } else {
+                            alert(`You tried to strike ${ghost.name} but missed, try again!`);
+                        }
+
+                        if (playerStrength === 2) {
+                            ghost.hp -= 3;
+                            alert(`You're getting stronger, you really clobbered ${ghost.name}!`);
+                        } else {
+                            alert(`You tried to strike ${ghost.name} but missed, try again!`);
+                        }
+
+                        if (playerStrength >= 3) {
+                            ghost.hp -= 4;
+                            alert(`You're so powerful, you super zonked ${ghost.name}!`);
+                        } else {
+                            alert(`You tried to strike ${ghost.name} but missed, try again!`);
+                        }
+
                     }
 
                 }
 
                 if (ghost.agility === 2) {
                     if (Math.random() < .25) { 
-                        ghost.hp--;
-                        alert(`You zapped ${ghost.name}!`);
-                    } else {
-                        alert(`You tried to strike ${ghost.name} but missed, try again!`);
-                    }
+                        if (playerStrength === 0) {
+                            ghost.hp--;
+                            alert(`You zapped ${ghost.name}!`);
+                        } else {
+                            alert(`You tried to strike ${ghost.name} but missed, try again!`);
+                        }
 
+                        if (playerStrength === 1) {
+                            ghost.hp -= 2;
+                            alert(`You're getting stronger, you really clobbered ${ghost.name}!`);
+                        } else {
+                            alert(`You tried to strike ${ghost.name} but missed, try again!`);
+                        }
+
+                        if (playerStrength === 2) {
+                            ghost.hp -= 3;
+                            alert(`You're getting stronger, you really clobbered ${ghost.name}!`);
+                        } else {
+                            alert(`You tried to strike ${ghost.name} but missed, try again!`);
+                        }
+
+                        if (playerStrength >= 3) {
+                            ghost.hp -= 4;
+                            alert(`You're so powerful, you super zonked ${ghost.name}!`);
+                        } else {
+                            alert(`You tried to strike ${ghost.name} but missed, try again!`);
+                        }
+
+                    }
+                        
                 }
 
 
-                if (ghost.dexterity === 1) {
-                    if (Math.random() < .2) {
-                        if (ghost.strength === 2) {
-                            playerHP -= 2;
-                        }
+                // if (ghost.dexterity === 1) {
+                //     if (Math.random() < .2) {
+                //         if (ghost.strength === 2) {
+                //             playerHP -= 2;
+                //         }
         
-                        if (ghost.strength === 3) {
-                            playerHP -= 3;
-                        }
+                //         if (ghost.strength === 3) {
+                //             playerHP -= 3;
+                //         }
         
-                        if (ghost.strength === 1) {
-                            playerHP --;
-                        }
-                        alert(`You got spooked by ${ghost.name}!`);
-                    } else {
-                        alert(`${ghost.name} tried to scare you but you're one tough doggie!`);
-                    }
-                    console.log(ghost.dexterity);
+                //         if (ghost.strength === 1) {
+                //             playerHP --;
+                //         }
+                //         alert(`You got spooked by ${ghost.name}!`);
+                //     } else {
+                //         alert(`${ghost.name} tried to scare you but you're one tough doggie!`);
+                //     }
+                // }
 
-                }
-
-                if (ghost.dexterity === 2) {
-                    if (Math.random() < .4) {
-                        if (ghost.strength === 2) {
-                            playerHP -= 2;
-                        }
+                // if (ghost.dexterity === 2) {
+                //     if (Math.random() < .4) {
+                //         if (ghost.strength === 2) {
+                //             playerHP -= 2;
+                //         }
         
-                        if (ghost.strength === 3) {
-                            playerHP -= 3;
-                        }
+                //         if (ghost.strength === 3) {
+                //             playerHP -= 3;
+                //         }
         
-                        if (ghost.strength === 1) {
-                            playerHP --;
-                        }
-                        alert(`You got spooked by ${ghost.name}!`);
-                    } else {
-                        alert(`${ghost.name} tried to scare you but you're one tough doggie!`);
-                    }
-                    console.log(ghost.dexterity);
-                }
-
+                //         if (ghost.strength === 1) {
+                //             playerHP --;
+                //         }
+                //         alert(`You got spooked by ${ghost.name}!`);
+                //     } else {
+                //         alert(`${ghost.name} tried to scare you but you're one tough doggie!`);
+                //     }
+                // }
                 
 
-                if (playerHP === 0) {
+                if (playerHP <= 0) {
                     dogImgEl.classList.add('game-over');
                     dogImgEl.src = './assets/stars.png';
                     alert('GAME OVER :(');
                 }
 
+                
+
                 if (ghost.hp === 0) {
                     defeatedGhostsCount++;
+                    playerStrength++;
                     eachGhost.removeAttribute('disabled');
                 }
 
                 
 
                 dogHPEl.textContent = playerHP;
+                dogStrengthEl.textContent = playerStrength;
                 defeatedNumEl.textContent = defeatedGhostsCount;
 
                 displayGhosts();
